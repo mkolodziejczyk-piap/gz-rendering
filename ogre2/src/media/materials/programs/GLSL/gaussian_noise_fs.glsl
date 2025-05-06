b95469dec2a05a9757996e5d89dfbe9371679537
Miron Kolodziejczyk <miron.kolodziejczyk@piap.lukasiewicz.gov.pl>
2025-05-06 19:00:38 +0200

    update

--- ogre2/src/media/materials/programs/GLSL/gaussian_noise_fs.glsl
+++ ogre2/src/media/materials/programs/GLSL/gaussian_noise_fs.glsl
@@ -1,61 +1,41 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #version ogre_glsl_ver_330
 
-//-------------------------------
-// Horizontal Gaussian-Blur pass
-//-------------------------------
+// The input texture, which is set up by the Ogre Compositor infrastructure.
+vulkan_layout( ogre_t0 ) uniform texture2D RT;
+vulkan( layout( ogre_s0 ) uniform sampler texSampler );
 
-vulkan_layout( ogre_t0 ) uniform texture2D Blur0;
-vulkan( layout( ogre_s0 ) uniform sampler samplerState );
+// Other parameters are set in C++, via
+// Ogre::GpuProgramParameters::setNamedConstant()
 
-vulkan_layout( location = 0 )
-out vec4 fragColour;
-
+// input params from vertex shader
 vulkan_layout( location = 0 )
 in block
 {
-	vec2 uv0;
+  vec2 uv0;
 } inPs;
 
-float offsets[11]  = float[11]
-(
-	-5.0, -4.0, -3.0, -2.0, -1.0,
-	 0.0,
-	 1.0,  2.0,  3.0,  4.0,  5.0
-);
-
-//We use the Normal-gauss distribution formula
-//f(x) being the formula, we used f(0.5)-f(-0.5); f(1.5)-f(0.5)...
-float samples[11] = float[11]
-(//stddev=2.0
-	0.01222447,
-	0.02783468,
-	0.06559061,
-	0.12097757,
-	0.17466632,
-
-	0.19741265,
-
-	0.17466632,
-	0.12097757,
-	0.06559061,
-	0.02783468,
-	0.01222447
-);
+// final output color
+vulkan_layout( location = 0 )
+out vec4 fragColor;
 
 void main()
 {
-	vec4 retVal;
-
-	vec2 uv = inPs.uv0.xy;
-	uv.x += offsets[0] * 0.01;
-	vec4 sum = texture( vkSampler2D( Blur0, samplerState ), uv ) * samples[0];
-
-	for( int i=1; i<11; ++i )
-	{
-		uv = inPs.uv0.xy;
-		uv.x += offsets[i] * 0.01;
-		sum += texture( vkSampler2D( Blur0, samplerState ), uv ) * samples[i];
-	}
-
-	fragColour = sum;
+  fragColor = clamp(0.2 * texture(vkSampler2D(RT,texSampler), inPs.uv0.xy) + 0.8 * vec4(0, 0.2, 0.5, 0.0), 0.0, 1.0);
 }
