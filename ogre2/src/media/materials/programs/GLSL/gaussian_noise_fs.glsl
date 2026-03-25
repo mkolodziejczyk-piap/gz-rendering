3fae34b43189db5795f59d05505cf9300d92fb11
Miron Kolodziejczyk <miron.kolodziejczyk@piap.lukasiewicz.gov.pl>
2026-03-25 13:15:22 +0100

    update

--- ogre2/src/media/materials/programs/GLSL/gaussian_noise_fs.glsl
+++ ogre2/src/media/materials/programs/GLSL/gaussian_noise_fs.glsl
@@ -65,6 +65,8 @@ void main()
 
   // if(depth == 0.0) depth=1.0;
 
+  depth = 0.00001 / (depth-0.00001)
+
   vec4 sceneColor = cb * sharp + (1-cb) * blur;
 	// fragColor = clamp(cd * sceneColor + (1-cd) * vec4(0, 0.2, 0.4, 0.0), 0.0, 1.0);
   fragColor = clamp(vec4(0, 0, depth, 0.0), 0.0, 1.0);
